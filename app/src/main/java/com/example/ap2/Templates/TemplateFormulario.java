64e53de2c3423ad86ff0bc763c3e464caaa9f84e
Bruce Dickson <95632543+BruceDicksonn@users.noreply.github.com>
2022-06-04 11:16:32 -0300

    metodos abstratos implementados

--- app/src/main/java/com/example/ap2/Templates/TemplateFormulario.java
+++ app/src/main/java/com/example/ap2/Templates/TemplateFormulario.java
@@ -1,4 +1,50 @@
 package com.example.ap2.Templates;
 
-public interface TemplateFormulario {
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.ap2.Dao.UsuariosDao;
+import com.example.ap2.Model.Usuario;
+
+public abstract class TemplateFormulario extends AppCompatActivity {
+
+    @Override
+    public abstract boolean onCreateOptionsMenu(Menu menu);
+
+    @Override
+    public abstract boolean onOptionsItemSelected(@NonNull MenuItem item);
+
+    protected abstract void alert(String message);
+
+    public abstract void initComponents();
+
+    protected abstract boolean verificarInputs();
+
+    protected Usuario getUsuarioLogado(String email, String senha){
+
+        Usuario retorno = new Usuario();
+
+        for(Usuario user : UsuariosDao.getListaUsuarios()){
+            if(user.getEmail().equals(email) && user.getSenha().equals(senha)){
+                retorno = user;
+            }
+        }
+
+        return retorno;
+
+    }
+
+    protected abstract void abrirTelaPrincipal();
+
+    protected abstract void abrirAreaAlunos();
+
+    protected abstract void abrirAreaProdutos();
+
+    protected abstract boolean validarEmailESenha();
+
+    protected abstract void limparInputs();
+
+
 }
